435893d7ca8eafc2bf8e90ed1b687e19fdebd071
isaac <isaacverm@gmail.com>
2022-04-05 14:58:47 +0200

    save phrases to csv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch()
@@ -12,20 +13,23 @@ const puppeteer = require('puppeteer');
         return [...document.querySelectorAll('.word > a')].map(element => element.getAttribute('href'))
     })
 
-    let phrases = []
+    const writeStream = fs.createWriteStream('phrases.csv')
+    writeStream.write('english,russian\n')
 
     for (const link of links) {
-        console.log(phrases.flat())
-
         await page.goto(link)
 
-        phrases.push(await page.evaluate(() => {
+        const phrases = await page.evaluate(() => {
             const russian = [...document.querySelectorAll('.phrase_plain .first')].map(element => element.innerText)
             const english = [...document.querySelectorAll('.phrase_plain .first + li')].map(element => element.innerText)
             return russian.map((a, i) => {
                 return { "russian": a, "english": english[i] }
             })
-        }))
+        })
+
+        phrases.forEach(phrase => {
+            writeStream.write(`${phrase.english}, ${phrase.russian}\n`)
+        })
     }
 
     await browser.close()
