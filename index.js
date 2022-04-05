d161824bb0d92b1622dcb534527f64a42153b242
isaac <isaacverm@gmail.com>
2022-04-05 13:52:14 +0200

    fetch russian phrases single word

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+    // const browser = await puppeteer.launch()
     const browser = await puppeteer.launch({ headless: false })
 
     const page = await browser.newPage()
@@ -10,6 +11,13 @@ const puppeteer = require('puppeteer');
     const link = await page.evaluate(() => {
         return document.querySelector('.word > a').getAttribute('href')
     })
+
+    await page.goto(link)
+
+    const russianPhrases = await page.evaluate(() => {
+        return [...document.querySelectorAll('.phrase_plain .first')].map(element => element.innerText)
+    })
+    
 
     await browser.close()
 })()
