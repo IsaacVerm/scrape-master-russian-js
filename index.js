2e2105ab00c41a789c8f843befd9d2f75a9893cc
isaac <isaacverm@gmail.com>
2022-04-06 10:26:48 +0200

    extract functions script

--- index.js
+++ index.js
@@ -3,51 +3,78 @@ const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch()
-    // const browser = await puppeteer.launch({ headless: false })
 
     const page = await browser.newPage()
 
-    // 11 cause there are 12 pages and the first page has no number
-    // +2 instead of +1 because you don't need a suffix for the first page and index starts at 0
-    const suffixes = ['', Array(11).fill().map((element, index) => `_${index + 2}`)].flat()
-    const initalLinks = suffixes.map(suffix => `http://masterrussian.com/vocabulary/most_common_words${suffix}.htm`)
+    function createInitialLinks() {
+        // Array(11) cause there are 12 pages and the first page has no number
+        // index +2 instead of +1 because you don't need a suffix for the first page and index starts at 0
+        const suffixes = ['', Array(11).fill().map((element, index) => `_${index + 2}`)].flat()
 
-    const phrasesStream = fs.createWriteStream('phrases.csv')
-    phrasesStream.write('english,russian\n')
+        return suffixes.map(suffix => `http://masterrussian.com/vocabulary/most_common_words${suffix}.htm`)
+    }
 
-    const errorStream = fs.createWriteStream('errors.csv')
-    errorStream.write('link,errorMessage\n')
+    function createStream(csv_path, column_names) {
+        const stream = fs.createWriteStream(csv_path)
+        stream.write(`${column_names}\n`)
+        return stream
+    }
 
-    for (const initialLink of initalLinks) {
-        await page.goto(initialLink)
-
-        const links = await page.evaluate(() => {
+    async function getWordLinks() {
+        return await page.evaluate(() => {
             return [...document.querySelectorAll('.word > a')].map(element => element.getAttribute('href'))
         })
+    }
+
+    function checkLink(link) {
+        if (!link.includes('http://masterrussian.com')) {
+            link = 'http://masterrussian.com' + link
+        }
+
+        return link
+    }
+
+    async function getPhrases() {
+       return await page.evaluate(() => {
+            const russian = [...document.querySelectorAll('.phrase_plain .first')].map(element => element.innerText)
+            const english = [...document.querySelectorAll('.phrase_plain .first + li')].map(element => element.innerText)
+            return russian.map((a, i) => {
+                return { "russian": a, "english": english[i] }
+            })
+        })
+    }
+
+    function writePhaseToStream(phrase, phrasesStream) {
+        phrasesStream.write(`"${phrase.english}", "${phrase.russian}"\n`)
+    }
+
+    function writeErrorToStream(link, error, errorStream) {
+        errorStream.write(`"${link}","${error.message}"\n`)
+    }
+
+    const phrasesStream = createStream('phrases.csv', 'english,russian')
+    const errorStream = createStream('errors.csv', 'link,errorMessage')
+
+    for (const initialLink of createInitialLinks()) {
+        await page.goto(initialLink)
+
+        const links = await getWordLinks()
 
         for (let link of links) {
             try {
                 console.log(link)
-                // if (!link.includes('http://masterrussian.com')) {
-                //     link = 'http://masterrussian.com' + link
-                // }
+                link = checkLink(link)
 
                 await page.goto(link)
 
-                const phrases = await page.evaluate(() => {
-                    const russian = [...document.querySelectorAll('.phrase_plain .first')].map(element => element.innerText)
-                    const english = [...document.querySelectorAll('.phrase_plain .first + li')].map(element => element.innerText)
-                    return russian.map((a, i) => {
-                        return { "russian": a, "english": english[i] }
-                    })
-                })
+                const phrases = await getPhrases()
 
                 phrases.forEach(phrase => {
-                    phrasesStream.write(`"${phrase.english}", "${phrase.russian}"\n`)
+                    writePhaseToStream(phrase, phrasesStream)
                 })
             }
             catch (error) {
-                errorStream.write(`"${link}","${error.message}"\n`)
+                writeErrorToStream(link, error, errorStream)
             }
         }
     }
